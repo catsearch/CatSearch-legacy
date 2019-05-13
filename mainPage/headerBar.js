62d9a020f28e8f0ced68549ed3a0355ef1b58c4b
mvj23 <michaelji2021@u.northwestern.edu>
2019-05-13 11:23:45 -0500

    Enter in search bar now triggers search function

--- mainPage/headerBar.js
+++ mainPage/headerBar.js
@@ -1,6 +1,8 @@
-let headerMenu = document.getElementById("header-dropdown-content")
+let headerMenu = document.getElementById("header-dropdown-content");
+let searchBar = document.getElementById("search-bar");
 let mouse = false;
 
+// search: backend call, not implemented
 function search() {
     console.log("wow, you searched");
 }
@@ -9,6 +11,8 @@ function mouseStatus(n) {
      mouse = n;
 }
 
+// closeDropdown: closes dropdown if open and mouse is not over the dropdown
+// Called when click off the dropdown
 function closeDropdown() {
     if (!mouse){
         if (headerMenu.style.display === "block") {
@@ -17,6 +21,8 @@ function closeDropdown() {
     }
 }
 
+// toggleDropdown: open or close dropdown menu
+// Called when clicking on actual text
 function toggleDropdown() {
     if (headerMenu.style.display === "block"){
         headerMenu.style.display = "none";
@@ -25,3 +31,13 @@ function toggleDropdown() {
         headerMenu.style.display = "block"; 
     }
 }
+
+searchBar.addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      search();
+    }
+});
