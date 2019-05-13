853c242c92fc994e34799999feb9d8a57187921c
mvj23 <michaelji2021@u.northwestern.edu>
2019-05-12 23:41:37 -0500

    Everything is probably fine except dropdown doesn't work on first click

--- mainPage/headerBar.js
+++ mainPage/headerBar.js
@@ -1,14 +1,27 @@
-headerMenu = document.getElementById("header-dropdown-content")
+let headerMenu = document.getElementById("header-dropdown-content")
+let mouse = false;
 
 function search() {
     console.log("wow, you searched");
 }
 
-function showDropdown() {
+function mouseStatus(n) {
+     mouse = n;
+}
+
+function closeDropdown() {
+    if (!mouse){
+        if (headerMenu.style.display === "block") {
+            headerMenu.style.display = "none";
+        }
+    }
+}
+
+function toggleDropdown() {
     if (headerMenu.style.display === "none"){
-        headerMenu.style.display = "initial";
+        headerMenu.style.display = "block";
     }
     else {
-        headerMenu.style.display = "none";        
+        headerMenu.style.display = "none"; 
     }
 }
