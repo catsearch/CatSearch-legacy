6afae398fac9406a1fe6464907e22e8da89857d9
Beth Mallon <bcool19992012@gmail.com>
2019-05-21 21:37:27 -0500

    about me outside box

--- userProfile/userProfile.js
+++ userProfile/userProfile.js
@@ -6,7 +6,7 @@ let user = null;
 let defaultUser = {
     firstName: "Michael",
     lastName: "Horn",
-    blurb: "This is Michael's blurb! Isn't it nice?",
+    blurb: "This is Michael's blurb! Isn't it nice? And I have a lot to fucking say about things, so much so that I really want this to overflow so I can figure out my goddamn shit and make sure that this formats how I want it ya know. Somebody once told me the world is gonna roll me I ain't the sharpest tool in the shed She was looking kind of dumb with her finger and her thumb In the shape of an on her forehead Well the years start coming and they don't stop coming Fed to the rules and I hit the ground running Didn't make sense not to live for fun Your brain gets smart but your head gets dumb So much to do, so much to see So what's wrong with taking the back streets? You'll never know if you don't go You'll never shine if you don't glow Hey now, you're an all-star, get your game on, go play Hey now, you're a rock star, get the show on, get paid And all that glitters is gold Only shooting stars break the mold It's a cool place and they say it gets colder You're bundled up now, wait till you get older But the meteor men beg to differ Judging by the hole in the satellite picture The ice we skate is getting pretty thin The water's getting warm so you might as well swim My world's on fire, how about yours? That's the way I like it and I never get bored Hey now, you're an all-star, get your game on, go play Hey now, you're a rock star, get the show on, get paid All that glitters is gold Only shooting stars break the mold Hey now, you're an all-star, get your game on, go play Hey now, you're a rock star, get the show, on get paid And all that glitters is gold Only shooting stars Somebody once asked could I spare some change for gas? I need to get myself away from this place I said yep what a concept I could use a little fuel myself And we could all use a little change Well, the years start coming and they don't stop coming Fed to the rules and I hit the ground running Didn't make sense not to live for fun Your brain gets smart but your head gets dumb So much to do, so much to see So what's wrong with taking the back streets? You'll never know if you don't go (go!) You'll never shine if you don't glow Hey now, you're an all-star, get your game on, go play Hey now, you're a rock star, get the show on, get paid And all that glitters is gold Only shooting stars break the mold And all that glitters is gold Only shooting stars break the mold",
     gender: "Male",
     school: "McCormick",
     year: "2022",
@@ -67,7 +67,6 @@ function buildDropDownFields() {
 
 function buildTimeFields() {
     let bedtimeField = document.createElement("span");
-    bedtimeField.setAttribute("id", "bedtime-field");;
     bedtimeField.setAttribute("class", "time-fields");
     bedtimeField.innerHTML = "Bedtime: ";
     bedtimeField.innerHTML += "From " + millitaryToRegular(defaultUser.bedtimeStart);
@@ -76,7 +75,6 @@ function buildTimeFields() {
 
     
     let wakeUpField = document.createElement("span");
-    wakeUpField.setAttribute("id", "wake-up-field");
     wakeUpField.setAttribute("class", "time-fields");
     wakeUpField.innerHTML = "Wake-Up: ";
     wakeUpField.innerHTML += "From " + millitaryToRegular(defaultUser.wakeUpStart);
@@ -85,14 +83,9 @@ function buildTimeFields() {
 }
 
 function buildBlurb() {
-    let aboutMe = document.createElement("span");
-    aboutMe.setAttribute("id", "blurb-about-me");
-    aboutMe.innerHTML = "About Me:";
-    userBlurbField.appendChild(aboutMe);
-
     let blurbField = document.createElement("p");
     blurbField.setAttribute("id", "blurb-field");
-    blurbField.innerHTML += defaultUser.blurb;
+    blurbField.innerHTML = defaultUser.blurb;
     userBlurbField.appendChild(blurbField);
 }
 
