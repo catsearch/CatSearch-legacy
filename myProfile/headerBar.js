b968d651bdacee8329fba64db5551bb1a6479600
mvj23 <michaelji2021@u.northwestern.edu>
2019-05-29 11:24:20 -0500

    dropdown on all three pages exists and works

---
+++ myProfile/headerBar.js
@@ -0,0 +1,107 @@
+const headerMenu = document.getElementById("header-dropdown");
+let dropdownText = null;
+let headerMenuContent = null;
+const searchBar = document.getElementById("search-bar");
+const userId = localStorage.getItem("userId");
+
+let mouse = false;
+
+// search: backend call, not implemented
+function search() {
+    if (searchBar.value !== "") {
+        localStorage.setItem("externalSearch", searchBar.value);
+    }
+    window.location = "../mainPage/mainPage.html";
+}
+
+function mouseStatus(n) {
+    mouse = n;
+}
+
+// closeDropdown: closes dropdown if open and mouse is not over the dropdown
+// Called when click off the dropdown
+function closeDropdown() {
+    if (!mouse && headerMenuContent){
+        if (headerMenuContent.style.display === "block") {
+            headerMenuContent.style.display = "none";
+        }
+    }
+}
+
+// toggleDropdown: open or close dropdown menu
+// Called when clicking on actual text
+function toggleDropdown() {
+    if (headerMenuContent) {
+        if (headerMenuContent.style.display === "block"){
+            headerMenuContent.style.display = "none";
+        }
+        else {
+            headerMenuContent.style.display = "block"; 
+        }
+    }
+}
+
+searchBar.addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      search();
+    }
+});
+
+function fetchUser() {
+    fetch(apiUrl + userId, {
+        method: "GET",
+        headers: apiHeader
+    })
+        .then(response => {
+            return response.json() 
+        })
+        .then(json => {
+            if (json.success) {
+                const user = json.user;
+                addDropdown(user.name.split(" ")[0]);
+                console.log(user);
+            } else {
+                addLoginButton();
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            addLoginButton();
+        })
+}
+
+function addDropdown(name) {
+    headerMenu.innerHTML = `
+        <span id="header-dropdown-text" onmouseover="mouseStatus(true);" onmouseout="mouseStatus(false);" onclick="toggleDropdown()"><u>${name? name : "User"}</u> &#x25BE</span>
+        <div id="header-dropdown-content" onmouseover="mouseStatus(true);" onmouseout="mouseStatus(false);">
+            <a href="../myProfile/myProfile.html">My Profile</a>
+            <a onclick="logout();">Log Out</a>
+        </div>`
+    dropdownText = document.getElementById("header-dropdown-text");
+    headerMenuContent = document.getElementById("header-dropdown-content");
+}
+
+function addLoginButton() {
+    headerMenu.innerHTML = `<div id="login-button"><span id="login-button-text" onclick="moveToLogin();">Login</span></div>`
+}
+
+function logout() { 
+    if (window.confirm("Are you sure you want to logout?")) {
+        localStorage.removeItem("userId");
+        moveToLogin();
+    }
+}
+
+function moveToLogin() {
+    window.location = "../login/login.html";
+}
+
+if (userId) {
+    fetchUser();
+} else {
+    addLoginButton();
+}
